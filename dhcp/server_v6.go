09c9e481a587435af18f65e97ea19e487ee667dd
Christian Mäder <mail@cimnine.ch>
2018-12-04 22:21:46 +0100

    Extracted IPv6 types

--- dhcp/server_v6.go
+++ dhcp/server_v6.go
@@ -6,12 +6,13 @@ import (
 	"log"
 	"net"
 
+	"github.com/google/gopacket/layers"
+	"github.com/satori/go.uuid"
+
 	"github.com/cimnine/netbox-dhcp/dhcp/config"
 	"github.com/cimnine/netbox-dhcp/dhcp/v6"
 	"github.com/cimnine/netbox-dhcp/dhcp/v6/consts"
 	"github.com/cimnine/netbox-dhcp/resolver"
-	"github.com/google/gopacket/layers"
-	"github.com/satori/go.uuid"
 )
 
 type ServerV6 struct {
@@ -25,34 +26,6 @@ type ServerV6 struct {
 }
 
 type dhcpv6OptMap map[layers.DHCPv6Opt]layers.DHCPv6Options
-
-type iaNontemporaryAddress struct {
-	iaid             [4]byte
-	t1               uint32
-	t2               uint32
-	addressOptions   []iaAddress
-	statusCodeOption statusCodeOption
-	otherOptions     []iaOpts
-}
-
-type iaOpts struct {
-	code uint16
-	data []byte
-}
-
-type iaAddress struct {
-	addr              net.IP
-	preferredLifetime uint32
-	validLifetime     uint32
-	statusCodeOption  statusCodeOption
-	otherOptions      []iaOpts
-}
-
-type statusCodeOption struct {
-	len           uint16
-	statusCode    uint16
-	statusMessage string
-}
 
 func NewServerV6(dhcpConfig *config.DHCPConfig, resolver resolver.Resolver, iface net.Interface, listenerConfig *config.V6ListenerConfig) (s ServerV6, err error) {
 	conn, err := v6.ListenDHCPv6(iface, listenerConfig.ListenToAddresses(), listenerConfig.ReplyFromAddress())
@@ -194,7 +167,7 @@ func (s *ServerV6) replyToSolicit(solicit layers.DHCPv6, srcIP net.IP, srcMAC ne
 	} else if len(clientInfo.IPAddrs) == 0 {
 		options = append(options,
 			statusOption(consts.DHCPv6StatusCodeNoAddressesAvailable, "Not yet implemented"))
-	} else if ianaOpts, hasIANA := optMap[layers.DHCPv6OptIANA]; hasIANA && !checkIANAs(ianaOpts, clientInfo) {
+	} else if ianaOpts, hasIANA := optMap[layers.DHCPv6OptIANA]; hasIANA && !v6.CheckIANAs(ianaOpts, clientInfo) {
 		options = append(options,
 			statusOption(consts.DHCPv6StatusCodeNotOnLink,
 				"According to this server's information some non-temporary IP addresses (IA_NA) are not designated for your machine."))
@@ -224,92 +197,6 @@ func (s *ServerV6) replyToSolicit(solicit layers.DHCPv6, srcIP net.IP, srcMAC ne
 
 	log.Printf("Sent a DHCPv6 ADVERTISE for client ID '%s' / MAC '%s' msg to '%s' ('%s')",
 		clientDUID, clientMAC, dstIP, dstMAC)
-}
-
-// checkIANAs returns true if all addresses of the given IANA option are also in the clientInfo
-func checkIANAs(ianaOpts layers.DHCPv6Options, clientInfo v6.ClientInfoV6) bool {
-	ianas := parseIANAOptions(ianaOpts)
-
-	for _, ipRequestedByClient := range ianas {
-		for _, ipOption := range ipRequestedByClient.addressOptions {
-			if !isIpInList(ipOption.addr, clientInfo.IPAddrs) {
-				return false
-			}
-		}
-	}
-	return true
-}
-
-// isIpInList checks if a given IP is in the given list of IPs and returns true if that's the case, false otherwise.
-func isIpInList(ip net.IP, list []net.IP) bool {
-	for _, ipOfList := range list {
-		if ip.Equal(ipOfList) {
-			return true
-		}
-	}
-	return false
-}
-
-// parseIANAOptions parses an IA Non-Temporary Address DHCPv6 Option
-func parseIANAOptions(ianaOpts layers.DHCPv6Options) []iaNontemporaryAddress {
-	var nontemporaryAddresses []iaNontemporaryAddress
-
-	for _, ianaOpt := range ianaOpts {
-		code := layers.DHCPv6Opt(binary.BigEndian.Uint16(ianaOpt.Data[:2]))
-
-		nontemporaryAddress := iaNontemporaryAddress{}
-		switch code {
-		case layers.DHCPv6OptIAAddr:
-			addrOpt := iaAddress{
-				addr:              ianaOpt.Data[4:20],
-				preferredLifetime: binary.BigEndian.Uint32(ianaOpt.Data[20:24]),
-				validLifetime:     binary.BigEndian.Uint32(ianaOpt.Data[24:28]),
-			}
-
-			nontemporaryAddress.addressOptions = append(nontemporaryAddress.addressOptions, addrOpt)
-
-			statusCodeOption, ok := findStatusCodeOpt(ianaOpt.Data[28:])
-			if ok {
-				nontemporaryAddress.statusCodeOption = statusCodeOption
-			}
-		case layers.DHCPv6OptStatusCode:
-			statusCode := statusCodeOption{
-				len:           binary.BigEndian.Uint16(ianaOpt.Data[2:4]),
-				statusCode:    binary.BigEndian.Uint16(ianaOpt.Data[4:6]),
-				statusMessage: string(ianaOpt.Data[6:]),
-			}
-
-			nontemporaryAddress.statusCodeOption = statusCode
-		}
-
-		nontemporaryAddresses = append(nontemporaryAddresses, nontemporaryAddress)
-	}
-
-	return nontemporaryAddresses
-}
-
-// findStatusCodeOpt browses through the given data to find a DHCPv6 Status Option. It does this recursively.
-// It returns the parsed option and true, if an option was found in the data.
-// It returns an empty option and false, if no option was found in the data.
-func findStatusCodeOpt(data []byte) (statusCodeOption, bool) {
-	if len(data) < 6 {
-		return statusCodeOption{}, false
-	}
-
-	endOfOption := binary.BigEndian.Uint16(data[2:4]) + 4 // 4 = status_code_len + opt_len,
-
-	optCode := layers.DHCPv6Opt(binary.BigEndian.Uint16(data[0:2]))
-	if optCode == layers.DHCPv6OptStatusCode {
-		option := statusCodeOption{
-			len:           binary.BigEndian.Uint16(data[2:4]),
-			statusCode:    binary.BigEndian.Uint16(data[4:6]),
-			statusMessage: string(data[6 : 4+endOfOption]),
-		}
-
-		return option, true
-	}
-
-	return findStatusCodeOpt(data[endOfOption:])
 }
 
 func (s *ServerV6) getClientLLAddr(clientLLAddrOpt layers.DHCPv6Options) net.HardwareAddr {
@@ -384,8 +271,6 @@ func (s *ServerV6) replyToRequest(request layers.DHCPv6, srcIP net.IP, srcMAC ne
 		return
 	}
 
-
-
 }
 
 // extractClientDUID returns the rawClientDUID for use in the response, and the clientDUID for use in a lookup
@@ -414,7 +299,7 @@ func isClientIDMissing(optMap dhcpv6OptMap, srcIP net.IP) bool {
 	if opts, found := optMap[layers.DHCPv6OptServerID]; found || len(opts) > 1 {
 		log.Printf("DHCPv6 message from '%s' contains %d server ID option(s).", srcIP, len(opts))
 	}
-	_, found := optMap[layers.DHCPv6OptClientID];
+	_, found := optMap[layers.DHCPv6OptClientID]
 	if !found {
 		log.Printf("DHCPv6 message from '%s' does not contain a client ID option. Discarding the message.", srcIP)
 		return true
